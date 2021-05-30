73a5cf047de5e5c2276bb12ffab123dfe64c9190
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-30 19:53:54 +0530

    initial commit

---
+++ AddressBookSystem.sql
@@ -0,0 +1,10 @@
+---Create table AddressBookSystem
+
+Create table AddressBookSystem(FirstName varchar(50),LastName varchar(50),Address varchar(50),City varchar(50), State varchar(50),Zip float,PhoneNumber float,EmailId varchar(50),AddressBookType varchar(50),AddressBookName varchar(50) );
+select * from AddressBookSystem;
+
+--insert record for table
+
+insert into AddressBookSystem values('Himanshu','kholiya','Wakad','Pune','Maharashtra','411001','7002285211','kholiyahimanshu2@gmail.com','Family','Vishal');
+select * from AddressBookSystem;
+
---
+++ Address_Book_System_ADO/AddressBookManagement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System_ADO
+{
+        public class AddressBookManagement
+        {
+            //UC1:- Address Book ServiceDB
+
+            public static string connectionString = @"Data Source=LAPTOP-BM4J1NMI;Initial Catalog=AddressBookSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; //Specifying the connection string from the sql server connection.
+
+            SqlConnection connection = new SqlConnection(connectionString); // Establishing the connection using the Sqlconnection.  
+
+            public void DataBaseConnection()
+            {
+                try
+                {
+                    DateTime now = DateTime.Now; //create object DateTime class //DateTime.Now class access system date and time 
+                    connection.Open(); // open connection
+                    using (connection)  //using SqlConnection
+                    {
+                        Console.WriteLine($"Connection is created Successful {now}"); //print msg
+
+                    }
+                    connection.Close(); //close connection
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }   
+}
